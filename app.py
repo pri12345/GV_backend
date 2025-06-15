e086b4a4da6a1e1dd0d88a127c50814b5780e0d8
pri12345 <pridagia@gmail.com>
2025-06-15 16:25:00 -0300

    adding DB classes in the new models.py file

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-# %%  Imports
+# %%  Imports and initial configurations
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+from models import db, Item, Sale  # Assuming models.py (in repo) contains the SQLAlchemy models
 
 app = Flask(__name__)
+CORS(app)
 
+#%% Database configuration (later switch to PostgreSQL)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-@app.route('/')
+#%% Routing
+
+@app.route('/')   # Simple front-end to visualize
 def home():
     return render_template('index.html')
 
@@ -14,4 +23,6 @@ def hello():
     return jsonify({"message": "Probando el backend con Flask"})
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock_quantity = db.Column(db.Integer, nullable=False, default=0)
+    description = db.Column(db.String(200), nullable=True)
+
+class Sale(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    sale_quantity = db.Column(db.Integer, nullable=False)
+    sale_timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    total_sale_value = db.Column(db.Float, nullable=False)

8cd8244e6746fe86ec3d242e04c25ee15e698378
pri12345 <pridagia@gmail.com>
2025-06-15 21:01:06 -0300

    new functions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # %%  Imports and initial configurations
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_cors import CORS
-from models import db, Item, Sale  # Assuming models.py (in repo) contains the SQLAlchemy models
+from models import db, Item, Sale, Stock_Addition  # Assuming models.py (in repo) contains the SQLAlchemy models
 
 app = Flask(__name__)
 CORS(app)
@@ -21,6 +21,8 @@ def home():
 def hello():
     return jsonify({"message": "Probando el backend con Flask"})
 
+## Lists Item, Sales, Stock Added
+
 @app.route('/items')
 def list_items():
     items = Item.query.all()
@@ -31,26 +33,36 @@ def list_sales():
     sales = Sale.query.all()
     return render_template('sales.html', sales=sales)
 
+@app.route('/stock_additions')
+def list_stock_additions():
+    stock_additions = Stock_Addition.query.all()
+    return render_template('stock_additions.html', stock_additions=stock_additions)
+
+# ADD ITEM and RECORD SALE
 
 @app.route('/add_item', methods=['GET'])
 def show_add_item_form():
     return render_template('add_item.html')
 
-
 @app.route('/add_item', methods=['POST'])
 def add_item():
+    id = request.form.get('id')  
     name = request.form['name']
     cost = float(request.form['cost'])
     price = float(request.form['price'])
-    stock_quantity = int(request.form['stock_quantity'])
+    stock = int(request.form['stock'])
     new_item = Item(
-                    name=name, cost=cost, price=price,
-                    stock_quantity=stock_quantity               
+                    id=id, name=name, cost=cost, price=price,
+                    stock=stock       
     )
     db.session.add(new_item)
     db.session.commit()
     return redirect(url_for('list_items'))
 
+
+@app.route('/record_sale', methods=['GET'])
+def show_record_sale_form():
+    return render_template('record_sale.html')
 
 @app.route('/record_sale', methods=['POST'])
 def record_sale():
@@ -58,17 +70,51 @@ def record_sale():
     quantity = int(request.form['quantity'])
     
     item = Item.query.get(item_id)
-    if item and item.quantity >= quantity:
+    if item and item.stock >= quantity:
         total = item.price * quantity
         new_sale = Sale(item_id=item_id, quantity=quantity, total=total)
         # Update the item's quantity
-        item.quantity -= quantity
+        item.stock -= quantity
         db.session.add(new_sale)
         db.session.commit()
         return redirect(url_for('list_sales'))
     else:
         return "Insufficient stock or item not found", 400
     
+
+## ADD STOCK
+
+@app.route('/add_stock', methods=['GET'])
+def show_add_stock_form():
+    return render_template('add_stock.html')
+
+@app.route('/add_stock', methods=['POST'])
+def add_stock():
+    item_id = int(request.form['item_id'])
+    additional_stock = int(request.form['additional_stock'])
+    
+    item = Item.query.get(item_id)
+    if item:
+        old_stock = item.stock
+        movement = Stock_Addition(item_id=item_id, additional_stock=additional_stock)
+        item.stock += additional_stock
+        db.session.add(movement)
+        db.session.commit()
+
+        return (
+                f"Succesfully added stock for item ID: {item_id}\n"
+                f"name: {item.name}\n old stock: {old_stock}\n new stock: {item.stock}\n "
+                f"movement number: {movement.id}\n movement timestamp: {movement.timestamp}"
+            )
+    
+    else:
+        return "Wrong Item ID, or inexistent Item", 400
+
+
+
+
+
+
 
 
 
@@ -77,3 +123,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+# %%
--- models.py
+++ models.py
@@ -7,12 +7,20 @@ class Item(db.Model):
     name = db.Column(db.String(100), nullable=False)
     cost = db.Column(db.Float, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    stock_quantity = db.Column(db.Integer, nullable=False, default=0)
+    stock = db.Column(db.Integer, nullable=False, default=0)
     description = db.Column(db.String(200), nullable=True)
 
 class Sale(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-    sale_quantity = db.Column(db.Integer, nullable=False)
-    sale_timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
-    total_sale_value = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    total = db.Column(db.Float, nullable=False)
+
+class Stock_Addition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    additional_stock = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
+
+    
